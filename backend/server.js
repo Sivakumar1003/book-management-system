a957b78d344a4feecab317e89d8ab585f20f2718
Sivakumar N <shivask94423@gmail.com>
2025-05-10 14:01:39 +0530

    addes user routes.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+const cors = require('cors');
 
+const userRouter = require("./src/routes/users.routes");
+
+const app = express();
+const PORT = process.env.URL_PORT || 5000;
+
+// Enable cors
+app.use(cors())
+
+// Parse incoming request bodies
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+// Routes
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to the home page." })
 })
+app.use('/api/users', userRouter)
 
+// Run server and log port number.
 app.listen(PORT, () => {
     console.log(`server is runing in ${PORT}`)
 })

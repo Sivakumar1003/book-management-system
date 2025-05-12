8de6afae8f966a48f4db9bdd17fc60e740eec2b0
Sivakumar N <shivask94423@gmail.com>
2025-05-12 20:19:15 +0530

    added new route to add book.

--- backend/src/routes/books.routes.js
+++ backend/src/routes/books.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { verifyToken } = require('../middlewares/verifyToken');
 const { roleAuthorization } = require('../middlewares/roleAuthorization');
-const { getAllBooks } = require('../controlers/books.controler');
+const { getAllBooks, addBook } = require('../controlers/books.controler');
 
 const router = express.Router();
 
 router.route('/allBooks').get(verifyToken, roleAuthorization(["admin", "user"]), getAllBooks);
+router.route('/addBook').post(verifyToken, roleAuthorization(["admin"]), addBook)
 
 module.exports = router;
